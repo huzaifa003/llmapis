75cc5aa5f81cfb9683ed0f72a6375c0d6ace1d72
Arsalan831 <arsalanbashir831@gmail.com>
2024-11-08 12:15:48 +0500

    update plan list

--- data/planList.js
+++ data/planList.js
@@ -1,75 +1,74 @@
 export const planList = {
-    "plans": [
-      {
-        "name": "Free-Tier",
-        "price": "Free",
-        "value": "free",
-        "features": [
-          "Limited access to over 100 AI Models over API",
-          "Limited token usage",
-          "Limited AI Playground model selection"
-        ]
-      },
-    //   {
-    //     "name": "Pro Weekly",
-    //     "price": "$4.99/week",
-    //     "value": "proWeekly",
-    //     "features": [
-    //       "2.5 Million AI/ML Tokens or 250 Images",
-    //       "Access to Pro models in the AI playground",
-    //       "Serverless access to over 100 AI Models"
-    //     ]
-    //   },
-      {
-        "name": "Pro Monthly",
-        "price": "$15.00/month",
-        "value": "proMonthly",
-        "features": [
-          "10 Million AI/ML Tokens or 1K Images",
-          "Access to Pro models in the AI playground",
-          "Serverless access to over 100 AI Models"
-        ]
-      },
-      {
-        "name": "Pro Yearly",
-        "price": "$150.00/year",
-        "value": "proYearly",
-        "features": [
-          "120 Million AI/ML Tokens or 12K Images",
-          "Access to Pro models in the AI playground",
-          "Serverless access to over 100 AI Models"
-        ]
-      },
-    //   {
-    //     "name": "Premium Weekly",
-    //     "price": "$7.00/week",
-    //     "value": "premiumWeekly",
-    //     "features": [
-    //       "5 Million AI/ML Tokens or 500 Images",
-    //       "Access to Premium models in the AI playground",
-    //       "Serverless access to over 100 AI Models"
-    //     ]
-    //   },
-      {
-        "name": "Premium Monthly",
-        "price": "$20.00/month",
-        "value": "premiumMonthly",
-        "features": [
-          "20 Million AI/ML Tokens or 2K Images",
-          "Access to Premium models in the AI playground",
-          "Serverless access to over 100 AI Models"
-        ]
-      },
-      {
-        "name": "Premium Yearly",
-        "price": "$200.00/year",
-        "value": "premiumYearly",
-        "features": [
-          "240 Million AI/ML Tokens or 24K Images",
-          "Access to Premium models in the AI playground",
-          "Serverless access to over 100 AI Models"
-        ]
-      }
-    ]
-  };
-  
+  "plans": [
+    {
+      "name": "Free-Tier",
+      "price": "Free",
+      "value": "free",
+      "features": [
+        "Limited access to over 100 AI Models over API",
+        "Limited token usage",
+        "Limited AI Playground model selection"
+      ]
+    },
+  //   {
+  //     "name": "Pro Weekly",
+  //     "price": "$4.99/week",
+  //     "value": "proWeekly",
+  //     "features": [
+  //       "2.5 Million AI/ML Tokens or 250 Images",
+  //       "Access to Pro models in the AI playground",
+  //       "Serverless access to over 100 AI Models"
+  //     ]
+  //   },
+    {
+      "name": "Pro Monthly",
+      "price": "$49.00/month",
+      "value": "proMonthly",
+      "features": [
+        "10 Million AI/ML Tokens or 1K Images",
+        "Access to Pro models in the AI playground",
+        "Serverless access to over 100 AI Models"
+      ]
+    },
+    {
+      "name": "Pro Yearly",
+      "price": "$499.00/year",
+      "value": "proYearly",
+      "features": [
+        "120 Million AI/ML Tokens or 12K Images",
+        "Access to Pro models in the AI playground",
+        "Serverless access to over 100 AI Models"
+      ]
+    },
+  //   {
+  //     "name": "Premium Weekly",
+  //     "price": "$7.00/week",
+  //     "value": "premiumWeekly",
+  //     "features": [
+  //       "5 Million AI/ML Tokens or 500 Images",
+  //       "Access to Premium models in the AI playground",
+  //       "Serverless access to over 100 AI Models"
+  //     ]
+  //   },
+    {
+      "name": "Business Monthly",
+      "price": "$199.00/month",
+      "value": "premiumMonthly",
+      "features": [
+        "20 Million AI/ML Tokens or 2K Images",
+        "Access to Premium models in the AI playground",
+        "Serverless access to over 100 AI Models"
+      ]
+    },
+    {
+      "name": "Premium Yearly",
+      "price": "$1999.00/year",
+      "value": "premiumYearly",
+      "features": [
+        "240 Million AI/ML Tokens or 24K Images",
+        "Access to Premium models in the AI playground",
+        "Serverless access to over 100 AI Models"
+      ]
+    }
+  ]
+};
